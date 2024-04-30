0780b314e8d2c93e10bec1dea76ec04dbb715aef
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2024-05-01 02:06:17 +0700

    Dynamic Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,28 @@
 <script>
-  import AutoFocus from "./components/AutoFocus.svelte";
+  import TabA from "./components/TabA.svelte";
+  import TabB from "./components/TabB.svelte";
+  import TabC from "./components/TabC.svelte";
+
+  let activeTab = TabA;
 </script>
 
 <main>
-  <AutoFocus />
+  <button on:click={() => (activeTab = TabA)}>Tab A</button>
+  <button on:click={() => (activeTab = TabB)}>Tab B</button>
+  <button on:click={() => (activeTab = TabC)}>Tab C</button>
+  <br>
+  <svelte:component this={activeTab}>
+
+  </svelte:component>
+  <!-- {#if activeTab === 'TabA'}
+    <TabA />
+  {/if}
+  {#if activeTab === 'TabB'}
+    <TabB />
+  {/if}
+  {#if activeTab === 'TabC'}
+    <TabC />
+  {/if} -->
 </main>
 
 <style>
---
+++ src/components/TabC.svelte
@@ -0,0 +1,1 @@
+Tab C Content
